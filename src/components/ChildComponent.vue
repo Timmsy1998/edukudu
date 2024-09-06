4ba72f77f2a9f1fe55b5db2a2d35ef502b61cacb
James Timms <59345798+Timmsy1998@users.noreply.github.com>
2024-09-06 10:38:50 +0100

    Built ChildComponent

--- src/components/ChildComponent.vue
+++ src/components/ChildComponent.vue
@@ -1,7 +1,31 @@
 <template>
     <div>
+        <h2>Child Component</h2>
+        <button @click="toggleBool">Toggle Bool</button>
+        <input type="text" v-model="localString" @input="updateString" />
     </div>
 </template>
 
 <script setup>
+import { ref, watch, defineProps, defineEmits } from 'vue';
+
+const props = defineProps({
+    parentData: Object
+});
+
+const emit = defineEmits(['toggleBool', 'updateString']);
+
+const localString = ref(props.parentData.stringVar);
+
+watch(() => props.parentData.stringVar, (newVal) => {
+    localString.value = newVal;
+});
+
+const toggleBool = () => {
+    emit('toggleBool');
+};
+
+const updateString = () => {
+    emit('updateString', localString.value);
+};
 </script>
