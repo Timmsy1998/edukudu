1600e14c81f0ff78e2849aeae494a897e0a81d86
James Timms <59345798+Timmsy1998@users.noreply.github.com>
2024-09-06 10:36:46 +0100

    Built Parent Component

--- src/components/ParentComponent.vue
+++ src/components/ParentComponent.vue
@@ -1,9 +1,26 @@
 <template>
     <div>
-        <ChildComponent />
+        <h1>Parent Component</h1>
+        <p>Boolean Value: {{ parentData.boolVar }}</p>
+        <p>String Value: {{ parentData.stringVar }}</p>
+        <ChildComponent :parentData="parentData" @toggleBool="toggleBool" @updateString="updateString" />
     </div>
 </template>
 
 <script setup>
+import { reactive } from 'vue';
 import ChildComponent from './ChildComponent.vue';
+
+const parentData = reactive({
+    boolVar: false,
+    stringVar: 'Hello'
+});
+
+const toggleBool = () => {
+    parentData.boolVar = !parentData.boolVar;
+};
+
+const updateString = (newString) => {
+    parentData.stringVar = newString;
+};
 </script>
